f7a0deaec862b063d9eb149ac66f0dca5bba4947
PeterCones <olly.john@live.co.uk>
2025-09-19 12:25:44 +0000

    Integrate django-allauth for user authentication; update settings and URLs, modify Budget and Goal models to adjust date fields, and add migration files

--- budgets/models.py
+++ budgets/models.py
@@ -6,6 +6,6 @@ from transactions.models import Category
 
 class Budget(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='budgets')
-    period_start = models.DateField(auto_now=True)
+    period_start = models.DateField()
     limit_amount = models.DecimalField(decimal_places=2, max_digits=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
