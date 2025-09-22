67eff0f03249a06ce142aecf5d8c4b6506e729d3
PeterCones <olly.john@live.co.uk>
2025-09-22 16:26:54 +0000

    Add transactions template extending base layout

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from django.views import generic
+from django.contrib import messages
+
+from django.http import HttpResponseRedirect
+
+from .models import Account, Transaction, Category
+
+
+
 # Create your views here.
+
+class TransactionList(generic.ListView):
+    queryset = Transaction.objects
+    template_name = 'transactions/transactions.html'
+    paginate_by = 4
