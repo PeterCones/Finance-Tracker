44a237428ebdebea0f40519b35168521fe8d91e5
PeterCones <olly.john@live.co.uk>
2025-09-24 12:40:41 +0000

    Update .gitignore, add budget views and templates, and modify base HTML for budget navigation

--- FinanceTracker/urls.py
+++ FinanceTracker/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('transactions/', include('transactions.urls'), name ='transactions-urls'),
     path('accounts/', include('allauth.urls')),
+    path('budgets/', include('budgets.urls'), name='budget-urls'),
     path('', include('dashboard.urls'), name='dashboard-urls'),
 ]
--- budgets/views.py
+++ budgets/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Budget
 
 # Create your views here.
+
+@login_required
+def budget(request):
+    
+    budgets = Budget.objects.filter(owner=request.user)
+    
+    template = 'budgets.html'
+    
+    return render(
+        request,
+        template,
+        {'budgets':budgets}
+    )
