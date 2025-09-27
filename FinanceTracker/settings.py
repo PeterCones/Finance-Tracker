4624705be266067fa7c658af9c63e8cabc726dc9
PeterCones <olly.john@live.co.uk>
2025-09-27 15:34:16 +0000

    Update ALLOWED_HOSTS for production and correct Procfile for Gunicorn

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -34,7 +34,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    'finance-tracker-production-ol-090f27f23bf4.herokuapp.com',
+    '.herokuapp.com', 
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
